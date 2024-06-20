83c2d012e2061ece0a1c7599e4e0cda39bbcdadf
Arvind Atmuri <arvind.atmuri@team.telstra.com>
2024-06-20 23:28:34 +1000

    Test Case 12 Completed

--- tests/test12.spec.js
+++ tests/test12.spec.js
@@ -20,7 +20,7 @@ test('Test Case 12: Add Products in Cart', async ({ page }) => {
     });
     
     
-    await test.step('Add First Product', async () => {
+    await test.step("Hover over first product and click 'Add to cart'", async () => {
         await page.locator('.productinfo > .btn').first().hover();
         await page.locator('.overlay-content > .btn').first().click();
     });
@@ -34,7 +34,7 @@ test('Test Case 12: Add Products in Cart', async ({ page }) => {
         
     });
 
-    await test.step('Add Second Product', async () => {
+    await test.step("Hover over second product and click 'Add to cart'", async () => {
         await page.locator('.productinfo > .btn').nth(1).hover();
         await page.locator('.overlay-content > .btn').nth(1).click();
     });
@@ -44,15 +44,29 @@ test('Test Case 12: Add Products in Cart', async ({ page }) => {
         await expect(page.getByText('Your product has been added')).toBeVisible();
         await expect(page.getByRole('link', { name: 'View Cart' })).toBeVisible();
         await expect(page.getByRole('button', { name: 'Continue Shopping' })).toBeVisible();
-        await page.getByRole('button', { name: 'Continue Shopping' }).click();
         
     });
 
-    await test.step('Click on Cart', async () => {
-        await page.getByRole('link', { name: ' Cart' }).click();
+    await test.step("Click 'View Cart' button", async () => {
+        await page.getByRole('link', { name: 'View Cart' }).click();
     });
-  
-  
+    
+    await test.step('Verify if both the products are added to cart', async () => {
+        let list_of_products = await page.$$("css=td.cart_product")
+        await expect(list_of_products.length).toBe(2)
+        await expect(page.locator('#product-1').getByRole('link', { name: 'Blue Top' })).toBeVisible();
+        await expect(page.locator('#product-2').getByRole('link', { name: 'Men Tshirt' })).toBeVisible();
+    });
+    
+    await test.step('Verify their prices, quantity and total price', async () => {
+        let list_of_products = await page.$$("css=td.cart_product")
+        for (let i = 1; i <= list_of_products.length; i++) {
+        await expect(page.locator(`#product-${i} > .cart_price`)).toBeVisible();
+        await expect(page.locator(`#product-${i} > .cart_quantity > .disabled`)).toBeVisible();
+        await expect(page.locator(`#product-${i} > .cart_total > .cart_total_price`)).toBeVisible();
+       }
+    });
+
 //   await expect(page.locator('#product-1')).toContainText('Blue Top');
 //   await expect(page.locator('#product-1')).toContainText('Rs. 500');
 //   await expect(page.locator('#product-1')).toContainText('Rs. 500');
